940530492e10ee7e3f6cffd3af9317ce47a8d89b
SayedHusain40 <s.husain_10@hotmail.com>
2025-10-05 17:41:04 +0300

    first commit

---
+++ lib/screens/roll_dice.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final normalizer = Random();
+
+class RollDice extends StatefulWidget {
+  const RollDice({super.key});
+
+  @override
+  State<RollDice> createState() => _RollDiceState();
+}
+
+class _RollDiceState extends State<RollDice> {
+  int diceRoller = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.deepPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/dice_images/dice-$diceRoller.png'),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    diceRoller = normalizer.nextInt(6) + 1;
+                  });
+                },
+                child: Text(
+                  "Roll Dice",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
